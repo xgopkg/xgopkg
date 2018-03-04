7f97bac1da59d6d467ccc8026c63c343db14c43a
XingYunLi <contact@idrmfly.com>
2018-03-04 22:04:46 +0800

    fixed bindata.go to be formated with gofmt -s

---
+++ pkg/assets/bindata_test.go
@@ -0,0 +1,19 @@
+package assets
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/logger.v1"
+)
+
+func init() {
+	log.SetOutputLevel(0)
+}
+func TestAsset(t *testing.T) {
+	data, err := Asset("swagger/dist/index.html")
+	if err != nil {
+		t.Error(err)
+	}
+	assert.NotEmpty(t, data)
+}
