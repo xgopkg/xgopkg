8d8b90ec7830682af6aa9c40f2e619335fb4dcdb
XingYunLi <contact@idrmfly.com>
2018-03-10 23:00:57 +0800

    update SQL

--- db/migrations/201803042326_add_package.up.sql
+++ db/migrations/201803042326_add_package.up.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `xgp_package` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `pkg_name` char(50) NOT NULL,
-  `pkg_source` varchar(100) NOT NULL,
-  `pkg_desc` varchar(255) DEFAULT NULL,
-  `updated_at` datetime NOT NULL,
-  `created_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE xgp_package (
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  pkg_name CHAR(50) NOT NULL,
+  pkg_source VARCHAR(100) NOT NULL,
+  pkg_desc VARCHAR(255) DEFAULT NULL,
+  updated_at DATETIME NOT NULL,
+  created_at DATETIME NOT NULL,
+  PRIMARY KEY (id)
 );
