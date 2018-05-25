f1badbd56757ec732193795574e58c4f7c790828
XingYunLi <contact@idrmfly.com>
2018-05-25 21:19:34 +0800

    frontend use ng 5

--- frontend/README.md
+++
@@ -1,4 +0,0 @@
-```
-$ npm install -g yarn
-$ yarn start
-```
--- frontend/config-overrides.js
+++
@@ -1,6 +0,0 @@
-module.exports = function override(config, env) {
-  const { injectBabelPlugin } = require('react-app-rewired');``
-  // do stuff with the webpack config...
-  config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
-  return config;
-};
